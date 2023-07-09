77df6169e9931230edbb68e842786f5e0e511feb
jonis100 <jonishei100@gmail.com>
2023-07-09 14:52:59 +0300

    Fix empty list, and add venv

---
+++ conf.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+TOR_INTERVAL = 7
+PASSWORDS_LEN = 8
+THREADS_NUM = 5
+USE_TOR = False
---
+++ page_detales.py
@@ -0,0 +1,13 @@
+'''
+fileds_names = {
+            "user_name": "wpforms[fields][1]",
+            "password": "wpforms[fields][2]",
+            "submit_button": "wpforms[submit]"
+}
+'''
+
+fileds_names = {
+            "user_name": "email",
+            "password": "pass",
+            "submit_button": "login"
+}
