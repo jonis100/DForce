48f8f71789eb6dc2cd1c050f186d2e50d1b3e06e
Yoni S <68191462+jonis100@users.noreply.github.com>
2023-07-09 15:24:46 +0300

    Delete page_detales.py

--- page_detales.py
+++
@@ -1,5 +0,0 @@
-fileds_names = {
-            "user_name": 'email', #"wpforms[fields][1]",
-            "password":  'pass', #"wpforms[fields][2]",
-            "submit_button": "login" #"wpforms[submit]"
-}
